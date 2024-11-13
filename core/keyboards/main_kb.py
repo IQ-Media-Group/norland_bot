4c5366e6785b584ae2094a40aa72fb969d46b529
Gyrman Daniil <gyrman01app@mail.ru>
2024-11-13 14:58:24 +0300

    remove register btn

--- core/keyboards/main_kb.py
+++ core/keyboards/main_kb.py
@@ -1,13 +1,9 @@
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder, KeyboardButton, ReplyKeyboardMarkup
-from core.db.scripts import get_user
 
 
 def main_kb_as_markup(tg_id: int) -> ReplyKeyboardMarkup:
     main_kb = ReplyKeyboardBuilder()
     main_kb.add(KeyboardButton(text='Пройти тест'))
-
-    if not get_user(tg_id):
-        main_kb.add(KeyboardButton(text='Регистрация'))
 
     main_kb.max_width = 2
 
